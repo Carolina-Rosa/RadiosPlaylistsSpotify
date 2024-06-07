fb3b293b6fd8422a046244e197c80ad7138c57df
Carolina Rosa <mcarolinarosa96@gmail.com>
2024-06-07 17:46:19 +0100

    file playlist service

--- SpotifyPlaylistRadio/Services/PlaylistService.cs
+++ SpotifyPlaylistRadio/Services/PlaylistService.cs
@@ -7,9 +7,14 @@ namespace SpotifyPlaylistRadio.Services
     public class PlaylistService
     {
         private readonly IMongoCollection<Playlist> _playlistCollection;
+        private readonly ISpotifyService _spotifyService;
+        private readonly RadiosService _radiosService;
+        private readonly Playlists _playlists;
+        private readonly AuthToken _authToken;
 
         public PlaylistService(
-            IOptions<SpotifyPlaylistsFromRadioDatabaseSettings> spotifyPlaylistsFromRadioDatabaseSettings)
+            IOptions<SpotifyPlaylistsFromRadioDatabaseSettings> spotifyPlaylistsFromRadioDatabaseSettings, ISpotifyService spotifyService, RadiosService radiosService, Playlists playlists, AuthToken authToken)
+
         {
             var mongoClient = new MongoClient(
                 spotifyPlaylistsFromRadioDatabaseSettings.Value.ConnectionURI);
@@ -18,22 +23,52 @@ namespace SpotifyPlaylistRadio.Services
                 spotifyPlaylistsFromRadioDatabaseSettings.Value.DatabaseName);
 
             _playlistCollection = mongoDatabase.GetCollection<Playlist>("Playlist");
+            _spotifyService = spotifyService;
+            _radiosService = radiosService;
+            _playlists = playlists;
+            _authToken = authToken;
         }
 
         public async Task<List<Playlist>> GetAsync() =>
             await _playlistCollection.Find(_ => true).ToListAsync();
 
         public async Task<Playlist?> GetAsync(string id) =>
-            await _playlistCollection.Find(x => x.id == id).FirstOrDefaultAsync();
+            await _playlistCollection.Find(x => x.ID == id).FirstOrDefaultAsync();
+        
+        public async Task<Playlist> GetPlaylistInfoFromRadioName(string radioName)
+        {
+
+            var radio = await _radiosService.GetByNameAsync(radioName);
+            if (radio == null)
+            {
+                return null;
+            }
+
+            var playlistName = "Listening " + radio.displayName;
+            Playlist playlist = _playlists.items.FirstOrDefault((item) => item.Name == playlistName);
+
+            if (playlist == null)
+            {
+                return null;
+            }
+
+            Playlist radioPlaylist = await _spotifyService.GetPlaylist(_authToken.access_token, playlist.ID);
+
+            if (radioPlaylist != null)
+            {
+                radioPlaylist.TracksTotal = radioPlaylist.Tracks.total;
+            }
+            return radioPlaylist;
+        }
 
         public async Task CreateAsync(Playlist newPlaylist) =>
             await _playlistCollection.InsertOneAsync(newPlaylist);
 
         public async Task UpdateAsync(string id, Playlist updatedPlaylist) =>
-            await _playlistCollection.ReplaceOneAsync(x => x.id == id, updatedPlaylist);
+            await _playlistCollection.ReplaceOneAsync(x => x.ID == id, updatedPlaylist);
 
         public async Task RemoveAsync(string id) =>
-            await _playlistCollection.DeleteOneAsync(x => x.id == id);
+            await _playlistCollection.DeleteOneAsync(x => x.ID == id);
 
     }
 
